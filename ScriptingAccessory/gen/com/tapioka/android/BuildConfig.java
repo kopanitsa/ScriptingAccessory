07cf75232149b329c7c83a255558c6f257dfa700
kopanitsa <okada.takahiro111@gmail.com>
2013-08-16 00:43:02 +0900

    merge script interpreter and serial communicator
    Merged ScriptForAndroidTemplate and usb-serial-for-android as ScriptingAccessory.
    When application start, 1st activity have 2 functions.
    1. user can select connected device to start serial communication
    2. user can start to run a script with pressing start button.
    After selected a device, user can send on/off event to device.
    1-1. When user press on/off button, android send serial event

---
+++ ScriptingAccessory/gen/com/tapioka/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.tapioka.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ ScriptingAccessory/src/com/tapioka/android/interpreter/Script.java
@@ -0,0 +1,39 @@
+// Copyright 2010 Google Inc. All Rights Reserved.
+
+package com.tapioka.android.interpreter;
+
+
+import com.tapioka.android.R;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+public class Script {
+
+    // load file name is described here
+    public final static int ID = R.raw.script;
+    // public final static int ID = R.raw.weather_light;
+
+  public static String sFileName;
+
+  public static String getFileName(Context context) {
+    if (sFileName == null) {
+      Resources resources = context.getResources();
+      String name = resources.getText(ID).toString();
+      sFileName = name.substring(name.lastIndexOf('/') + 1, name.length());
+    }
+    return sFileName;
+  }
+
+  public static String getFileExtension(Context context) {
+    if (sFileName == null) {
+      getFileName(context);
+    }
+    int dotIndex = sFileName.lastIndexOf('.');
+    if (dotIndex == -1) {
+      return null;
+    }
+    return sFileName.substring(dotIndex);
+  }
+
+}
